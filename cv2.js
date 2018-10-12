ac77eb3dd546c8d2c65e841cd07d7605e70f1392
james <charlie592009@outlook.fr>
2018-10-12 16:15:48 +0200

    cv2

---
+++ cv2.js
@@ -0,0 +1,52 @@
+function bouton() {
+
+    var x = document.getElementById('mydiv');
+
+    if (x.style.display === 'none') {
+
+        x.style.display = 'block';
+
+        document.getElementById('bouton').innerHTML = '-';
+
+    } else {
+
+        x.style.display = 'none';
+
+        document.getElementById('bouton').innerHTML = '+';    }
+
+}
+function bouton2 () {
+
+    var x = document.getElementById('mydiv2');
+
+    if (x.style.display === 'none') {
+
+        x.style.display = 'block';
+
+        document.getElementById('bouton2').innerHTML = '-';
+
+    } else {
+
+        x.style.display = 'none';
+
+        document.getElementById('bouton2').innerHTML = '+';    }
+
+}
+
+function bouton3 () {
+
+    var x = document.getElementById('mydiv3');
+
+    if (x.style.display === 'none') {
+
+        x.style.display = 'block';
+
+        document.getElementById('bouton3').innerHTML = '-';
+
+    } else {
+
+        x.style.display = 'none';
+
+        document.getElementById('bouton3').innerHTML = '+';    }
+
+}
